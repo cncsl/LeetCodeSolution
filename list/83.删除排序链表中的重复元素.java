1338a3dab86572a316b6574bd7f5745331b1200b
Cn_Nullptr <justitacsl@outlook.com>
2019-09-07 10:43:19 +0800

    2019.09.06

---
+++ list/83.删除排序链表中的重复元素.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=83 lang=java
+ *
+ * [83] 删除排序链表中的重复元素
+ */
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode(int x) { val = x; } }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode prevNode = head;
+        ListNode currNode = head.next;
+
+        while (currNode != null) {
+            if (prevNode.val == currNode.val) {
+                prevNode.next = currNode.next;
+                currNode = currNode.next;
+            } else {
+                prevNode = prevNode.next;
+                currNode = currNode.next;
+            }
+        }
+        return head;
+    }
+}
