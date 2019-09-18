2664130223a393d99b122369a29cf446895204bc
Cn_Nullptr <justitacsl@outlook.com>
2019-09-18 22:50:22 +0800

    2019.09.18

---
+++ tree/112.路径总和.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=112 lang=java
+ *
+ * [112] 路径总和
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null)
+            return false;
+
+        //叶子结点
+        if(root.left == null && root.right == null && root.val == sum)
+            return true;
+   
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+}
+
