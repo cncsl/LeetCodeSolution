743cfff702776793dd9b6a7ba7d06e914b27c7f6
Cn_Nullptr <justitacsl@outlook.com>
2019-11-02 18:59:56 +0800

    20191102

---
+++ tree/226.翻转二叉树.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=java
+ *
+ * [226] 翻转二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        if (root.left != null || root.right != null) {
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+        }
+        root.left = invertTree(root.left);
+        root.right = invertTree(root.right);
+        return root;
+    }
+}
+// @lc code=end
