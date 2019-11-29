ce86bf07735e4d3e5be010bc17fd6486e016a611
Cn_Nullptr <justitacsl@outlook.com>
2019-11-29 22:03:25 +0800

    20191129

---
+++ 509.斐波那契数.java
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=509 lang=java
+ *
+ * [509] 斐波那契数
+ */
+
+// @lc code=start
+class Solution {
+    public int fib(int N) {
+        int first = 0;
+        int second = 1;
+        for(int i = 0; i < N; i++){
+            second = second + first;
+            first = second - first;
+        }
+        return first;
+    }
+}
+// @lc code=end
