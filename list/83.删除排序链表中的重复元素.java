8c144b2928fe3f21e9e2e6663aec3966e7e937b9
Cn_Nullptr <justitacsl@outlook.com>
2020-02-26 22:46:59 +0800

    20200226

--- list/83.删除排序链表中的重复元素.java
+++ list/83.删除排序链表中的重复元素.java
@@ -9,20 +9,26 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        if (head == null || head.next == null)
+        if (head == null || head.next == null) {
             return head;
-
-        ListNode prevNode = head;
-        ListNode currNode = head.next;
-        while (currNode != null) {
-            if (prevNode.val == currNode.val) {
-                prevNode.next = currNode.next;
-                currNode = currNode.next;
-            } else {
-                prevNode = prevNode.next;
-                currNode = currNode.next;
-            }
         }
-        return head;
+        ListNode vHead = new ListNode(-1);
+        vHead.next = head;
+        ListNode slow = vHead;
+        ListNode fast = head;
+
+        while (fast != null) {
+            if ((fast.next != null && fast.val != fast.next.val) || fast.next == null) {
+                if (slow.next == fast) {
+                    slow = fast;
+                } else {
+                    // slow 是重复元素的前一个（slow自身不是）
+                    // fast 是重复元素的最后一个（fast自身也是）
+                    slow.next = fast;
+                }
+            }
+            fast = fast.next;
+        }
+        return vHead.next;
     }
 }
---
+++ tree/513.找树左下角的值.java
@@ -0,0 +1,40 @@
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode.cn id=513 lang=java
+ *
+ * [513] 找树左下角的值
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        // 题干说明 root 不为 null
+        int result = 0;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            // 每次开始循环，queue的第一个元素都是当前行最左边的结点
+            result = queue.peek().val;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+        return result;
+    }
+}
+// @lc code=end
