4f09d034f4288d4a209962c01da7a3c51b627770
Cn_nullptr <justitacsl@outlook.com>
2020-09-20 20:29:27 +0800

    JUST

---
+++ 78.子集.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=78 lang=java
+ *
+ * [78] 子集
+ */
+
+// @lc code=start
+class Solution {
+    List<Integer> t = new ArrayList<Integer>();
+    List<List<Integer>> ans = new ArrayList<List<Integer>>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+        int n = nums.length;
+        for (int mask = 0; mask < (1 << n); ++mask) {
+            t.clear();
+            for (int i = 0; i < n; ++i) {
+                if ((mask & (1 << i)) != 0) {
+                    t.add(nums[i]);
+                }
+            }
+            ans.add(new ArrayList<Integer>(t));
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
