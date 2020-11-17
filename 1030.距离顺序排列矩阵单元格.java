c36f31f7d13a42a57971d46d9021beaaef257d59
nullptr <justitacsl@outlook.com>
2020-11-17 22:38:29 +0800

    Java8

---
+++ 1030.距离顺序排列矩阵单元格.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=1030 lang=java
+ *
+ * [1030] 距离顺序排列矩阵单元格
+ */
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+// @lc code=start
+class Solution {
+    public int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
+        int[][] ret = new int[R * C][];
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                ret[i * C + j] = new int[]{i, j};
+            }
+        }
+        Arrays.sort(ret, Comparator.comparingInt(a -> (Math.abs(a[0] - r0) + Math.abs(a[1] - c0))));
+        return ret;
+    }
+}
+// @lc code=end
+
