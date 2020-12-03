29980c216bbfdfa794370ebdfaa0995b7f0c97c5
Chen Shilong <justitacsl@outlook.com>
2020-12-03 13:30:37 +0800

    70k+

---
+++ 204.计数质数.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=204 lang=java
+ *
+ * [204] 计数质数
+ */
+
+// @lc code=start
+class Solution {
+    public int countPrimes(int n) {
+        int result = 0;
+        loop : for (int i = 2; i < n; i++) {
+            for (int j = 2; j * j <= i; j++) {
+                //余数为零不是质数
+                if (i % j == 0) {
+                    continue loop;
+                }
+            }
+            result++;
+        }
+        return result;
+    }
+}
+// @lc code=end
+
