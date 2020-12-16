e2a211e721a217417840d2526af3c3f4723235a8
nullptr <justitacsl@outlook.com>
2020-12-16 22:58:17 +0800

    Six month

---
+++ hashmap/290.单词规律.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=290 lang=java
+ *
+ * [290] 单词规律
+ */
+
+// @lc code=start
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        char[] charArr = pattern.toCharArray();
+        String[] strArr = s.split(" ");
+        if (charArr.length != strArr.length) {
+            return false;
+        }
+
+        //char key、string value and string key、char value
+        Map<Character, String> cksv = new HashMap<>();
+        Map<String, Character> skcv = new HashMap<>();
+
+        int sindex = 0;
+        for (char ch : charArr) {
+            String str = strArr[sindex++];
+            //如果存在字母到单词的映射，检查单词到字母的映射是否正确，下方相反
+            if (cksv.containsKey(ch) && !str.equals(cksv.get(ch))) {
+                return false;
+            }
+            if (skcv.containsKey(str) && skcv.get(str) != ch) {
+                return false;
+            }
+            cksv.put(ch, str);
+            skcv.put(str, ch);
+        }
+        return true;
+    }
+}
+// @lc code=end
+
