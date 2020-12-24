d0e7b9e6cbd7841e1ca23a14df2fa5ce6687be33
nullptr <justitacsl@outlook.com>
2020-12-24 23:53:18 +0800

    merry christmas

---
+++ tree/965.单值二叉树.java
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode.cn id=965 lang=java
+ *
+ * [965] 单值二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+    public boolean isUnivalTree(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int signalValue = root.val;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.val != signalValue) {
+                    return false;
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+        return true;
+    }
+    
+}
+// @lc code=end
+
