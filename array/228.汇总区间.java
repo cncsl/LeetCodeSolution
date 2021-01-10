d2200fb8da6772abb08d79927dca0592dfea1129
nullptr <justitacsl@outlook.com>
2021-01-10 13:51:00 +0800

    41471

---
+++ array/228.汇总区间.java
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=228 lang=java
+ *
+ * [228] 汇总区间
+ */
+
+// @lc code=start
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> ret = new ArrayList<String>();
+        int i = 0;
+        int n = nums.length;
+        while (i < n) {
+            int low = i;
+            i++;
+            while (i < n && nums[i] == nums[i - 1] + 1) {
+                i++;
+            }
+            int high = i - 1;
+            StringBuffer temp = new StringBuffer(Integer.toString(nums[low]));
+            if (low < high) {
+                temp.append("->");
+                temp.append(Integer.toString(nums[high]));
+            }
+            ret.add(temp.toString());
+        }
+        return ret;
+    }
+}
+// @lc code=end
+
---
+++ tree/1325.删除给定值的叶子节点.java
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=1325 lang=java
+ *
+ * [1325] 删除给定值的叶子节点
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        if (root == null) {
+            return root;
+        }
+        root.left = removeLeafNodes(root.left, target);
+        root.right = removeLeafNodes(root.right, target);
+
+        if (root.val == target && root.left == null && root.right == null) {
+            return null;
+        }
+        return root;
+    }
+}
+// @lc code=end
+
