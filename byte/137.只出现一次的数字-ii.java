9b2e857ed07c8aac78d143928545cf641081a95e
cncsl <justitacsl@outlook.com>
2021-04-30 09:35:24 +0800

    ctrl + v

---
+++ byte/137.只出现一次的数字-ii.java
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=137 lang=java
+ *
+ * [137] 只出现一次的数字 II
+ */
+
+// @lc code=start
+class Solution {
+    public int singleNumber(int[] nums) {
+        int a = 0, b = 0;
+        for (int num : nums) {
+            b = ~a & (b ^ num);
+            a = ~b & (a ^ num);
+        }
+        return b;
+    }
+}
+// @lc code=end
+
